6d1f0d50e112e478170055e6873b42a948a40a8c
ZoeGranitto <zoegranitto@gmail.com>
2024-01-10 17:38:44 -0300

    bin parser correcto

--- bin_parser.c
+++ bin_parser.c
@@ -1,107 +1,80 @@
-#include "bin_parser.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "common.h"
+#include <string.h>
+/* #include "utils.h"
+#include "command.h"
+#include "memcached.h" */
 
 #define MAX_TOKS 2
 
-int bin_consume(int fd)
+enum code bin_parser(char *buf, char *toks[], int lens[])
 {
-  int ntoks;
-  char *buf = malloc(10000);
-  int blen = 0;
-  char *toks[2] = {NULL};
-  int lens[2] = {0};
-  while (1)
-  {
-    int rem = sizeof *buf - blen;
-    assert(rem >= 0);
-
-    /* Buffer lleno, no hay comandos, matar */
-    if (rem == 0)
-      return -1;
-    int nread = READ(fd, buf + blen, rem);
-
-    log(3, "Read %i bytes from fd %i", nread, fd);
-    blen += nread;
-    char *p, *p0 = buf;
-    int nlen = blen;
-
-    ntoks = bin_parser(fd, buf, toks, lens);
-  }
-  return 0;
-}
-
-int bin_parser(int fd, char *buf, char *toks[], int lens[])
-{
-  enum code code = buf[0];
-  int ntoks;
-  printf("checking '%s'\n", code_str(code));
+  enum code command = buf[0];
+  printf("checking '%s'\n", code_str(command));
   int idx = 1;
-  switch (code)
-  {
-  case PUT:
-    ntoks = 2;
-    for (int i = 0; i < ntoks; i++)
-    {
-      memcpy(lens + i, buf + idx, 4);
-      lens[i] = ntohl(lens[i]); // cambia de formato big endian a little endian
+
+  if (command != PUT && command != GET && command != DEL && command != STATS) command = EINVALID;
+
+  else if (command == STATS);
+
+  else {
+    memcpy(lens, buf + idx, 4);
+    lens[0] = ntohl(lens[0]); // cambia de formato big endian a little endian
+    idx += 4;
+    toks[0] = buf + idx;
+    idx += lens[0];
+    if (command == PUT) {
+      memcpy(lens + 1, buf + idx, 4);
+      lens[1] = ntohl(lens[1]); // cambia de formato big endian a little endian
       idx += 4;
-      toks[i] = buf + idx;
-      idx += lens[i];
+      toks[1] = buf + idx;
+      idx += lens[1];
     }
-    break;
-  case GET:
-    ntoks = 1;
-    for (int i = 0; i < ntoks; i++)
-    {
-      memcpy(lens + i, buf + idx, 4);
-      lens[i] = ntohl(lens[i]); // cambia de formato big endian a little endian
-      idx += 4;
-      toks[i] = buf + idx;
-      idx += lens[i];
-    }
-    break;
-  case DEL:
-    ntoks = 1;
-    for (int i = 0; i < ntoks; i++)
-    {
-      memcpy(lens + i, buf + idx, 4);
-      lens[i] = ntohl(lens[i]); // cambia de formato big endian a little endian
-      idx += 4;
-      toks[i] = buf + idx;
-      idx += lens[i];
-    }
-    break;
-  default:
-    ntoks = 0;
-    break;
   }
+  return command;
+}
+
+
+int main(){
+  char* toks[MAX_TOKS];
+  for (int i = 0; i < MAX_TOKS; i++)
+    toks[i] = malloc(sizeof(char)* 1024);
+  int lens[MAX_TOKS];
+
+  /* creamos el archivo */
+  // Abrir el archivo en modo de escritura binaria
+  FILE *archivo = fopen("get_e.bin", "rb");
+  // Obtener el tamaño del archivo
+  fseek(archivo, 0, SEEK_END);
+  long tamañoArchivo = ftell(archivo);
+  char *buf = malloc(sizeof(char)*tamañoArchivo);  
+  rewind(archivo);
+  fread(buf, 1, tamañoArchivo, archivo);
+  fclose(archivo);
+  // Leer todos los datos en un buffer
+  // Cerrar el archivo
+  enum code command = bin_parser(buf, toks, lens);
   return 0;
 }
 
-void bin_handle(enum code command, char *toks[MAX_TOKS], int lens[MAX_TOKS])
-{
-  switch (command)
-  {
-  case PUT:
-    put(cache, queue, toks[2], toks[1]);
-    break;
-  case GET:
-    get(cache, queue, toks[1]);
-    break;
-  case DEL:
-    del(cache, queue, toks[1]);
-    break;
-  case STATS:
-    get_stats(cache);
-    break;
-  default:
-    break;
-  }
-}
+/*
 
-int main()
-{
-  int fd = open("mensajes/get_e.bin", 0);
-  printf("fd: %d\n", fd);
-  int r = bin_consume(fd);
-  return 0;
-}
+  // Datos binarios que deseas escribir en el archivo
+  unsigned char datos[] = {0x11};
+  // Escribir los datos en el archivo
+  size_t escritos = fwrite(datos, sizeof(unsigned char), sizeof(datos) / sizeof(unsigned char), archivo);
+
+  int entero = 1;
+  fwrite(&entero, sizeof(int), 1, archivo);
+  
+  unsigned char datos[] = {0x4A};
+  size_t escritos = fwrite(datos, sizeof(unsigned char), sizeof(datos) / sizeof(unsigned char), archivo);
+  
+  int entero = 2;
+  fwrite(&entero, sizeof(int), 1, archivo);
+  
+  unsigned char datos[] = {0x42, 0x00};
+  // Escribir los datos en el archivo
+  size_t escritos = fwrite(datos, sizeof(unsigned char), sizeof(datos) / sizeof(unsigned char), archivo);
+*/
