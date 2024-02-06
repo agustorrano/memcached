4ecf3bbdd194d7094c3375b817699808e530a143
agustorrano <agustorrano2003@gmail.com>
2024-02-06 12:11:18 -0300

    cambios client

--- client.erl
+++ client.erl
@@ -1,5 +1,5 @@
 -module(client).
--export([start/1, put/3, get/2, del/2, stats/1]).
+-export([start/1, put/3, get/2, del/2, stats/1, close/1]).
 
 -define(PUT, 11).
 -define(DEL, 12).
@@ -16,22 +16,19 @@
 % Crea un proceso que va a representar a un cliente el cual le
 % enviará mensajes al servidor y esperará respuestas.
 start(HostName) ->
-  case gen_tcp:connect(HostName, 8889, [binary, {active, false}]) of
+  case gen_tcp:connect(HostName, 8889, [binary, {active, false}, {packet, raw}]) of
     {ok, Socket} -> spawn(fun() -> client(Socket) end);
-    {error, Reason} -> Reason
+    {error, Error} -> Error
   end.
 
 
 client(Socket) ->
   receive
-    {put, K, V, Pid} -> Res = send_put(Socket, K, V),
-                        Pid ! Res;
-    {get, K, Pid} -> Res = send_get(Socket, K),
-                     Pid ! Res;
-    {del, K, Pid} -> Res = send_del(Socket, K),
-                     Pid ! Res;
-    {stats, Pid} -> Res = send_stats(Socket),
-                    Pid ! Res
+    {put, K, V, Pid} -> Pid ! send_put(Socket, K, V);
+    {get, K, Pid} -> Pid ! send_get(Socket, K);
+    {del, K, Pid} -> Pid ! send_del(Socket, K);
+    {stats, Pid} -> Pid ! send_stats(Socket);
+    close -> gen_tcp:close(Socket)
   end,
   client(Socket).
 
@@ -52,21 +49,21 @@ decode(Cmd) ->
     <<?EBIG>> -> ebig;
     <<?EUNK>> -> eunk;
     <<?EOOM>> -> eoom;
-    _ -> error
+    _ -> unknown
   end.
 
 
 answer(Socket) ->
   case gen_tcp:recv(Socket, 1) of
-    {ok, Cmd} -> {ok, decode(Cmd)};
-    Error -> Error
+    {ok, Cmd} -> decode(Cmd);
+    {error, Error} -> throw(Error)
   end.
 
 
 answer_data(Socket) ->
   case gen_tcp:recv(Socket, 4) of
     {ok, BSize} -> gen_tcp:recv(Socket, binary:decode_unsigned(BSize));
-    Error -> Error
+    {error, Error} -> throw(Error)
   end.
 
 
@@ -74,54 +71,76 @@ send_put(Socket, K, V) ->
   BKey = encode(K),
   BValue = encode(V),
   Req = <<?PUT, BKey/binary, BValue/binary>>,
-  gen_tcp:send(Socket, Req),
-  answer(Socket).
+  case gen_tcp:send(Socket, Req) of
+    ok -> answer(Socket);
+    {error, Error} -> throw(Error)
+  end.
 
 
 send_get(Socket, K) ->
   BKey = encode(K),
   Req = <<?GET, BKey/binary>>,
-  gen_tcp:send(Socket, Req),
-  case answer(Socket) of
-    {ok, ok} -> 
-      case answer_data(Socket) of
-        {ok, BValue} -> {ok, ok, binary_to_term(BValue)};
-        Error -> Error
+  case gen_tcp:send(Socket, Req) of
+    ok -> 
+      case answer(Socket) of
+        ok -> 
+          case answer_data(Socket) of
+            {ok, BValue} -> {ok, binary_to_term(BValue)};
+            {error, Error} -> throw(Error)
+          end;
+        Cmd -> Cmd
       end;
-    {ok, Cmd} -> {ok, Cmd};
-    Error -> Error
+    {error, Error} -> throw(Error)
   end.
 
 
 send_del(Socket, K) ->
   BKey = encode(K),
   Req = <<?DEL, BKey/binary>>,
-  gen_tcp:send(Socket, Req),
-  answer(Socket).
+  case gen_tcp:send(Socket, Req) of
+    ok -> answer(Socket);
+    {error, Error} -> throw(Error)
+  end.
 
 
 send_stats(Socket) ->
   Req = <<?STATS>>,
-  gen_tcp:send(Socket, Req),
-  case answer(Socket) of
-    {ok, ok} -> 
-      case answer_data(Socket) of
-        {ok, Bin} -> {ok, ok, binary_to_list(Bin)};
-        Error -> Error
+  case gen_tcp:send(Socket, Req) of
+    ok ->
+      case answer(Socket) of
+        ok -> 
+          case answer_data(Socket) of
+            {ok, Bin} -> {ok, Bin};
+            {error, Error} -> throw(Error)
+          end;
+        Cmd -> Cmd
       end;
-    {ok, Cmd} -> {ok, Cmd};
-    Error -> Error
+    {error, Error} -> throw(Error)
   end.
 
 
 put(Id, K, V) ->
-  Id ! {put, K, V, self()}.
+  Id ! {put, K, V, self()},
+  receive
+    Ans -> Ans
+  end.
 
 get(Id, K) ->
-  Id ! {get, K, self()}.
+  Id ! {get, K, self()},
+  receive
+    Ans -> Ans
+  end.
 
 del(Id, K) ->
-  Id ! {del, K, self()}.
+  Id ! {del, K, self()},
+  receive
+    Ans -> Ans
+  end.
 
 stats(Id) ->
-  Id ! {stats, self()}.
+  Id ! {stats, self()},
+  receive
+    Ans -> Ans
+  end.
+
+close(Id) -> Id ! close.
